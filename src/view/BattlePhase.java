7ad4e146a612148b523c501937b35fb277d4bf7c
APinCan <6794ch@naver.com>
2019-06-05 00:35:11 +0900

    bgm몇개 추가

--- src/view/BattlePhase.java
+++ src/view/BattlePhase.java
@@ -1,5 +1,50 @@
 package view;
 
-public class BattlePhase {
+import java.awt.Graphics;
 
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class BattlePhase extends JPanel implements View {
+	private int viewX=556;
+	private int viewY=371;
+	private String mapName;
+	ImageIcon icon;
+	
+	public BattlePhase() {
+		
+	}
+	
+	public BattlePhase(String mapName) {
+		this.mapName=mapName;
+		setMapSize(mapName);
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		g.drawImage(icon.getImage(), 0, 0, null);
+		
+		setOpaque(false);
+		super.paintComponent(g);
+	}
+	
+	@Override
+	public void setMapSize(String mapName) {
+		String imagePath=path+"/src/"+mapName+".jpg";
+		icon=new ImageIcon(imagePath);
+		
+		setSize(viewX, viewY);
+	}
+
+	@Override
+	public int getBackgroundImageX() {
+		// TODO Auto-generated method stub
+		return this.viewX;
+	}
+
+	@Override
+	public int getBackgroundImageY() {
+		// TODO Auto-generated method stub
+		return this.viewY;
+	}
 }
---
+++ src/view/GeneralView.java
@@ -0,0 +1,33 @@
+package view;
+
+import java.awt.Graphics;
+
+public class GeneralView implements View {
+	protected int viewX;
+	protected int viewY;
+	
+	@Override
+	public void setMapSize(String mapName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getBackgroundImageX() {
+		// TODO Auto-generated method stub
+		return viewX;
+	}
+
+	@Override
+	public int getBackgroundImageY() {
+		// TODO Auto-generated method stub
+		return viewY;
+	}
+
+}
